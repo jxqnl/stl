fb7c431a486737717f1536873933f9c32d618c87
jxqnl <270228823@qq.com>
2023-05-07 16:39:00 +0800

    const_iterator->iterator

--- effective stl/all_iterator/all_iterator/源.cpp
+++ effective stl/all_iterator/all_iterator/源.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <deque>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
-	//4种
+	//4种迭代器 iterator(T*),const_iterator(const T*),
+	//reverse_iterator(T*),const_reverse_iterator(const T*)
+	//istreambuf_iterator比istream_iterator更有效率,从缓冲区读取而不是格式化输入
+
+	//base()可以将reverse_iterator转成iterator 相差一个单位 insert不影响 earse影响
+	//distance()和advance()可以将const_iterator转换为iterator(不能强制转换,不能隐式转换)
+	//避免混合使用不同类型的迭代器
+
 	typedef deque<int> int_deque;
 	typedef int_deque::iterator int_iter;
 	using const_int_iter = int_deque::const_iterator;
@@ -13,5 +23,29 @@ int main(int argc, char** argv)
 		cout << "==" << endl;
 	else
 		cout << "!=" << endl;
+
+	//int_iter re_i(ci);
+	//int_iter re_i(const_cast<int_iter>(ci));
+
+	typedef vector<int> int_vec;
+	typedef int_vec::iterator int_vec_iter;
+	typedef int_vec::const_iterator int_const_vec_iter;
+
+	int_const_vec_iter ci_v;
+	//int_vec_iter i_v(const_cast<int_vec_iter>(ci_v));
+
+	typedef string::iterator str_iter;
+	typedef string::const_iterator const_str_iter;
+	const_str_iter ci_si;
+	//str_iter si(const_cast<str_iter>(ci_si));
+
+	int_deque d;
+	for(int i = 1; i <= 5; i++)
+		d.push_back(i);
+	const_int_iter d_const_iter = d.end();
+	int_iter d_iter(d.begin());
+	advance(d_iter,distance<const_int_iter>(d_iter, d_const_iter));
+
+	std::cout << *--d_iter << std::endl;
 	system("pause");
 }
